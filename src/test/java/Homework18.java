e580f4ab99c35ee06fb96c8b59b197b53a5af70d
vickuta01 <victoria.mereneanu@testpro.io>
2024-10-11 12:39:19 -0500

    homework18
    homework18 succceful

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+    @Test
+    public void playSong() {
+        navigateToPage();
+        provideEmail("victoria.mereneanu@testpro.io");
+        providePassword("Testpro1234$");
+        clickLoginBtn();
+        nextSong();
+        play();
+        Assert.assertTrue(isSongPlaying());
+
+    }
+public void nextSong() {
+    WebElement nextSongBtn = driver.findElement(By.cssSelector("i[data-testid='play-next-btn']"));
+    nextSongBtn.click();
+}
+public void play() {
+        WebElement playBtn = driver.findElement(By.cssSelector("div[class='side player-controls']"));
+        playBtn.click();
+}
+ public boolean isSongPlaying() {
+     WebElement pauseBtn = driver.findElement(By.cssSelector("span[class='pause']"));
+     return pauseBtn.isDisplayed();
+ }
+
+}
