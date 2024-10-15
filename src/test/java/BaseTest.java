0b4ef8f26e634ed844c54261e7e8e8b32d6427e8
vickuta01 <victoria.mereneanu@testpro.io>
2024-10-15 12:09:41 -0500

    HOMEWORK19

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,15 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 public class BaseTest {
 
@@ -8,4 +17,58 @@ public class BaseTest {
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+        public String url = null;
+        public WebDriver driver;
+
+
+
+
+
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void lauchClass(String BaseURL) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        url= BaseURL;
+
+    }
+
+    public void navigateToPage(){
+
+        driver.get(url);
+    }
+    public void provideEmail(String email) {
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void  providePassword(String password){
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+
+    }
+
+    public void  clickLoginBtn() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+
+    }
+
+
+
+
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+
 }
---
+++ src/test/java/Homework19.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        String expectedSuccessfulMsg = "Deleted playlist \"sprint 1\"";
+        navigateToPage();
+        provideEmail("victoria.mereneanu@testpro.io");
+        providePassword("Testpro1234$");
+        clickLoginBtn();
+        selectPlaylist();
+        xPlaylist();
+        Assert.assertEquals(expectedSuccessfulMsg, succesfulDeleteMsg());
+    }
+
+    public void selectPlaylist() {
+        WebElement playlist = driver.findElement(By.xpath("//li[@class='playlist playlist'][3]"));
+        playlist.click();
+    }
+    public void xPlaylist() {
+        WebElement xPlaylistBtn = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
+        xPlaylistBtn.click();
+    }
+    public String succesfulDeleteMsg(){
+        WebElement message = driver.findElement(By.cssSelector("div.success.show"));
+        return message.getText();
+    }
+}
