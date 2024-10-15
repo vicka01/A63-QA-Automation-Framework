a247e5e028b8d6fe9451eade30daace7ecb006ea
vickuta01 <victoria.mereneanu@testpro.io>
2024-10-15 12:36:27 -0500

    homework 19
    keep getting an error

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -6,19 +6,26 @@ import org.testng.annotations.Test;
 public class Homework19 extends BaseTest{
     @Test
     public void deletePlaylist() throws InterruptedException {
-        String expectedSuccessfulMsg = "Deleted playlist \"sprint 1\"";
+        String expectedSuccessfulMsg = "Deleted playlist \"sprint 1.\"";
         navigateToPage();
         provideEmail("victoria.mereneanu@testpro.io");
         providePassword("Testpro1234$");
         clickLoginBtn();
         selectPlaylist();
+        okToDelete();
         xPlaylist();
         Assert.assertEquals(expectedSuccessfulMsg, succesfulDeleteMsg());
     }
 
+
+
     public void selectPlaylist() {
         WebElement playlist = driver.findElement(By.xpath("//li[@class='playlist playlist'][3]"));
         playlist.click();
+    }
+    public void okToDelete() {
+        WebElement okBtn = driver.findElement(By.xpath("//div[@class='alertify']//button[@class= 'ok']"));
+        okBtn.click();
     }
     public void xPlaylist() {
         WebElement xPlaylistBtn = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
